4201e0856e863b577b2a949bb5b295dc3fc9b679
joao.barata <joao.barata@outsystems.com>
2021-11-04 17:37:57 +0000

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # cordova-plugin-android-webview-dark-mode
-Apache Cordova plugin to enable dark mode support for Android WebView's
+
+#### Apache Cordova plugin to enable dark mode support for Android WebView's
+
+### Goals
+
+In order for the Android WebView's to support dark modem we need to explicitly enable it.
+This will allow the webview to properly use the `(prefers-color-scheme: dark)` media query.
+
+For that we need to use the `Configuration.UI_MODE_NIGHT_YES`, `WebSettingsCompat.setForceDark` and `WebSettingsCompat.setForceDarkStrategy` configurations.
+You can read more here: https://developer.android.com/guide/webapps/dark-theme
+
+### Supported platforms
+
+-   **Android**
+
+#### Prerequisites/Warnings
+
+AndroidX is required. Therefore only **cordova-android >= 9.0.0** is supported.  
+You need to enable AndroidX in your `config.xml` by setting `AndroidXEnabled` to `true`.
+
+On your application you will need to add a metatag with the value `<meta name="color-scheme" content="dark light">`
+https://web.dev/color-scheme/#the-color-scheme-meta-tag
+
+As for how to handle the dark theme on your application, please refer to this article:
+https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
+
+---
+
+Built by (c) João Barata. Released under the MIT license.
---
+++ src/android/jnb/cordova/AndroidWebviewDarkMode.java
@@ -0,0 +1,53 @@
+package jnb.cordova;
+
+import android.content.res.Configuration;
+import android.util.Log;
+import androidx.webkit.WebSettingsCompat;
+import androidx.webkit.WebViewFeature;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.engine.SystemWebView;
+
+import static org.apache.cordova.CordovaActivity.TAG;
+import static androidx.webkit.WebSettingsCompat.FORCE_DARK_OFF;
+import static androidx.webkit.WebSettingsCompat.FORCE_DARK_ON;
+import static androidx.webkit.WebSettingsCompat.DARK_STRATEGY_WEB_THEME_DARKENING_ONLY;
+
+public class AndroidWebviewDarkMode extends CordovaPlugin {
+
+    @Override
+    protected void pluginInitialize() {
+        super.pluginInitialize();
+        Log.d(TAG, "Initializing DarkMode plugin");
+        checkDarkMode();
+        Log.d(TAG, "DarkMode plugin Enabled");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "Recheck DarkMode settings");
+        checkDarkMode();
+        Log.d(TAG, "DarkMode settings applied");
+    }
+
+    private void checkDarkMode() {
+
+        android.webkit.WebSettings settings = ((SystemWebView)webView.getEngine().getView()).getSettings();
+
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            switch ( cordova.getContext().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+                case Configuration.UI_MODE_NIGHT_YES:
+                    WebSettingsCompat.setForceDark( settings, FORCE_DARK_ON);
+                    break;
+                case Configuration.UI_MODE_NIGHT_NO:
+                case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                default:
+                    WebSettingsCompat.setForceDark(settings, FORCE_DARK_OFF);
+                    break;
+            }
+        }
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK_STRATEGY)){
+            WebSettingsCompat.setForceDarkStrategy( settings, DARK_STRATEGY_WEB_THEME_DARKENING_ONLY);
+        }
+    }
+}
